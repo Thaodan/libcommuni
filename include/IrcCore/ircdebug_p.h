9d4d707a132f5c11c6f8a224270147c79393db43
Björn Bidar <bjorn.bidar@thaodan.de>
2021-10-18 02:29:27 +0300

    Define wildcard expression manually

--- include/IrcCore/ircdebug_p.h
+++ include/IrcCore/ircdebug_p.h
@@ -143,7 +143,7 @@ static bool irc_debug_enabled(IrcConnection* c, uint l)
         dbg_init = true;
     }
 
-    return l <= dbg_level && (dbg_name.isEmpty() || QRegularExpression(QRegularExpression::wildcardToRegularExpression(dbg_name), QRegularExpression::CaseInsensitiveOption).match(c->displayName()).hasMatch());
+    return l <= dbg_level && (dbg_name.isEmpty() || QRegularExpression(dbg_name+".*", QRegularExpression::CaseInsensitiveOption).match(c->displayName()).hasMatch());
 }
 
 #define ircDebug(Connection, Flag) IrcDebug(Connection, Flag)
